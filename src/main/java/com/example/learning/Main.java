d63cbfdb7b53c9f8eba3542e86f0fc27648fd701
BharaN95 <nerellabharat@gmail.com>
2024-05-12 19:43:34 -0400

    Day 1 Sunday

---
+++ src/main/java/com/example/learning/Main.java
@@ -0,0 +1,30 @@
+package com.example.learning;
+
+public class Main {
+    public static void main(String agrs[]){
+        System.out.println("Hellow World");
+        System.out.println("I Love to Code");
+        int marks;
+        marks = 25;
+        System.out.println("Total Marks are :" + marks);
+
+        byte b = 10;
+        System.out.println(b);
+
+        short s = 23;
+        System.out.println(s);
+
+        int i = 42;
+        System.out.println(i);
+
+        long l = 456;
+        System.out.println(l);
+
+        float f = -3.6F;
+        System.out.println(f);
+
+        double d = 2.7;
+        System.out.println(d);
+
+    }
+}
---
+++ src/main/java/com/example/learning/Strings.java
@@ -0,0 +1,18 @@
+package com.example.learning;
+
+public class Strings {
+    public static void main (String args[]){
+        String s = "I love Java";
+        System.out.println(s);
+        System.out.println(s.charAt(4));
+
+        java.lang.String movieList = """ 
+                Harry Potter is Good
+                Harry Potter is gdgs
+                Harry Potter is gdfgsd
+                Harry Potter is sgsdg
+                Harry Potter is sgdsgsd
+                """;
+        System.out.println(movieList);
+    }
+}
